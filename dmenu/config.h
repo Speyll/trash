2591c220d2c28eef03866767f09ddb65a878ef7d
Lyes <44736944+Speyll@users.noreply.github.com>
2021-08-28 17:01:41 +0100

    Add files via upload

---
+++ dmenu/config.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Roboto Mono:style=Medium:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#0c0c0c", "#f2f2f2" },
+	[SchemeSel] = { "#f2f2f2", "#0c0c0c" },
+	[SchemeOut] = { "#0c0c0c", "#f2f2f2" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 24;
+static unsigned int min_lineheight = 24;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
