69c021fea4ffc6a4946d2c1cabac4b17766c944c
Lyes <44736944+Speyll@users.noreply.github.com>
2021-09-03 18:00:29 +0100

    Update launch.sh

--- .config/lemonbar/launch.sh
+++ .config/lemonbar/launch.sh
@@ -2,4 +2,4 @@
 background="#0c0c0c"
 color1="#f2f2f2"
 
-$HOME/.config/lemonbar/bar.sh | lemonbar -b -f "Roboto Mono:Regular:size=12" -F $color1 -B $background -g x24 -n "lemon" &
+$HOME/.config/lemonbar/bar.sh | lemonbar -f "monospace:Regular:size=12" -F $color1 -B $background -g x24 -n "lemon" &
