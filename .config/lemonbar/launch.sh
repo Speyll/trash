d1225b6f49466c0f83b84455b42fa4db460bfe8b
Lyes <44736944+Speyll@users.noreply.github.com>
2021-09-21 23:59:46 +0100

    gruvbox

--- .config/lemonbar/launch.sh
+++ .config/lemonbar/launch.sh
@@ -3,7 +3,7 @@ pkill -x lemonbar
 while pgrep -u $UID -x lemonbar >/dev/null; do sleep 1; done
 
 # Colors
-background="#0c0c0c"
-color1="#f2f2f2"
+background="#282828"
+color1="#ebdbb2"
 
 $HOME/.config/lemonbar/bar.sh | lemonbar -f "monospace:Regular:size=12" -F $color1 -B $background -g x24 -n "lemon" &
