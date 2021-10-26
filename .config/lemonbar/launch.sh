62ccfe5a5c81ff523bdc9f6ae21f468bf358251f
Speyll <Speyll@protonmail.com>
2021-10-26 14:37:11 +0100

    update

--- .config/lemonbar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-pkill -x lemonbar
-while pgrep -u $UID -x lemonbar >/dev/null; do sleep 1; done
-
-# Colors
-background="#282828"
-color1="#ebdbb2"
-
-$HOME/.config/lemonbar/bar.sh | lemonbar -f "monospace:Regular:size=12" -F $color1 -B $background -g x24 -n "lemon" &
